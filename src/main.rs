9d2ea98a48d28b361c6746e7feb51cf79265330b
Chris Patuzzo <chris@patuzzo.co.uk>
2025-06-21 11:35:16 +0100

    Send a null terminated string after each message

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,18 @@
 use std::net::TcpListener;
+use std::io::prelude::*;
+use std::io::BufReader;
 
 fn main() {
     let listener = TcpListener::bind("127.0.0.1:3003").unwrap();
 
-    for _ in listener.incoming() {
+    for result in listener.incoming() {
+        let stream = result.unwrap();
+        let mut writer = stream.try_clone().unwrap();
+        let mut buffer = vec![];
+        let mut reader = BufReader::new(&stream);
 
+        while reader.read_until(0, &mut buffer).is_ok() {
+            writer.write_all("\0".as_bytes()).unwrap();
+        }
     }
 }
